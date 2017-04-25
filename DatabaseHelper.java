74ee68d2f9a26930838fb7f49ca714678feb03c2
Liam-Horgan <14164736@studentmail.ul.ie>
2017-04-25 16:41:21 +0100

    added workout table

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -17,6 +17,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_3 = "WEIGHT";
     public static final String COL_4 = "ACTIVITY_LEVEL";
 
+    public static final String TABLE_NAME2 ="Workout_Table";
+    public static final String COL_5 = "WORKOUT_NAME";
+    public static final String COL_6 = "INTENSITY";
+    public static final String COL_7 = "LENGTH";
+    public static final String COL_8 = "CALORIES_BURNED"
+
 
 
 
@@ -28,11 +34,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, HEIGHT INTEGER, WEIGHT INTEGER, ACTIVITY_LEVEL INTEGER)");
+        db.execSQL("CREATE TABLE"+ TABLE_NAME2+"(WORKOUT_NAME VARCHAR, INTENSITY VARCHAR, LENGTH INTEGER, CALORIES_BURNED INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME2);
         onCreate(db);
     }
 
@@ -49,7 +57,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else
             return true;
     }
-
+    public boolean insertData2(String workout_name, String intensity, int length, int calories_burned){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_5, workout_name);
+        contentValues.put(COL_6, intensity);
+        contentValues.put(COL_7, length);
+        contentValues.put(COL_8, calories_burned)
+        long result = db.insert(TABLE_NAME2, null, contentValues);
+        if (result == -1){
+            return false;
+        }
+        else
+            return true;
+    }
 
 
 }
