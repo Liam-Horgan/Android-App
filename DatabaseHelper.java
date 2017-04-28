fe8d5c94c660906add43e478456bfe7d73f7a5fa
Liam-Horgan <14164736@studentmail.ul.ie>
2017-04-28 13:19:41 +0100

    Add files via upload

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -7,30 +7,37 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "Stats.db";
-    public static final String TABLE_NAME = "Stats_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "HEIGHT";
-    public static final String COL_3 = "WEIGHT";
-    public static final String COL_4 = "ACTIVITY_LEVEL";
 
-    public static final String TABLE_NAME2 ="Workout_Table";
-    public static final String COL_5 = "WORKOUT_NAME";
-    public static final String COL_6 = "INTENSITY";
-    public static final String COL_7 = "LENGTH";
-    public static final String COL_8 = "CALORIES_BURNED"
+    // Logcat tag
+    private static final String LOG = "DatabaseHelper";
+    //DB Version
+    private static final int DATABASE_VERSION = 1;
+    //Database proper
+    private static final String DATABASE_NAME = "Stats.db";
+    private static final String TABLE_NAME = "Stats_table";
+    public static final String COL_1 = "ID";
+    private static final String COL_2 = "HEIGHT";
+    private static final String COL_3 = "WEIGHT";
+    private static final String COL_4 = "ACTIVITY_LEVEL";
+
+    private static final String TABLE_NAME2 ="Workout_Table";
+    private static final String COL_5 = "WORKOUT_NAME";
+    private static final String COL_6 = "INTENSITY";
+    private static final String COL_7 = "LENGTH";
+    private static final String COL_8 = "CALORIES_BURNED";
 
 
 
 
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, HEIGHT INTEGER, WEIGHT INTEGER, ACTIVITY_LEVEL INTEGER)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, HEIGHT INTEGER, WEIGHT INTEGER, ACTIVITY_LEVEL INTEGER)");
         db.execSQL("CREATE TABLE"+ TABLE_NAME2+"(WORKOUT_NAME VARCHAR, INTENSITY VARCHAR, LENGTH INTEGER, CALORIES_BURNED INTEGER)");
     }
 
@@ -41,22 +48,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData(int height, int weight, int activityLevel, String workout_name, String intensity, int length, int calories_burned){
+    public boolean insertUser(int height, int weight, int activityLevel){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, height);
         contentValues.put(COL_3, weight);
         contentValues.put(COL_4, activityLevel);
-        contentValues.put(COL_5, workout_name);
-        contentValues.put(COL_6, intensity);
-        contentValues.put(COL_7, length);
-        contentValues.put(COL_8, calories_burned)
         long result = db.insert(TABLE_NAME, null, contentValues);
-        long result = db.insert(TABLE_NAME2, null, contentValues);
         if (result == -1){
             return false;
         }
         else
             return true;
+    }
+
+    public boolean insertWorkout(String workout_name, String intensity, int length, int calories_burned){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_5, workout_name);
+        contentValues.put(COL_6, intensity);
+        contentValues.put(COL_7, length);
+        contentValues.put(COL_8, calories_burned);
+        long result2 = db.insert(TABLE_NAME2, null, contentValues);
+        if (result2 == -1){
+            return false;
+        }
+        else
+            return true;
+    }
+
+    // closing database
+    public void closeDB() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen())
+            db.close();
     }
 }
